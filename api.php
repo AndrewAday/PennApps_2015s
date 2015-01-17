10d57f3ea90743fa3a6b33dfa5ac7d561a8d9674
Andrew Aday <andrewladay@gmail.com>
2015-01-16 22:58:08 -0500

    oops

--- api.php
+++ api.php
@@ -3,7 +3,7 @@
 ini_set("display_errors", true);
 error_reporting(E_ALL);
 
-require_once '/var/www/pennapps.gomurmur.com/php-echonest-api/EchoNest/Autoloader.php';
+require_once '/var/www/pennapps.gomurmur.com/php-echonest-api/lib/EchoNest/Autoloader.php';
 EchoNest_Autoloader::register();
 
 
